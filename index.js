693ceeade3191ab5b57500292e8e8358fff3543d
Ruslan Sulieimanov <ruslansulieim@gmail.com>
2021-06-21 22:14:47 +0300

    веб_САНДРА_21.06.21

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 // import './js/module-1-2.js'
 // import './js/practic-ls_1.js'
 // import './js/practic-ls_2.js'
-import './js/massif_repeta.js'
+// import './js/massif_repeta.js'
+// import './js/module-2-1-arrays.js'
+import './tasks/module_1.js'
+
 
 
