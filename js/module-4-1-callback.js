37b482e13ae2cb4e21da7781c27c1ab93f4119ce
Ruslan Sulieimanov <ruslansulieim@gmail.com>
2021-07-13 22:09:48 +0300

    tasks

--- js/module-4-1-callback.js
+++ js/module-4-1-callback.js
@@ -215,6 +215,7 @@
 // console.log(secondInvoke);
 // console.log(service.mailingList); 
 
+//////// 6 task 5 module ///////////////
 
 // function Storage(items) {
 // 	this.items = items;
@@ -223,10 +224,10 @@
 // 	return this.items;
 // };
 // Storage.prototype.addItem = function(newItem){
-// 	this.items = newItem;
+// 	this.items.push(newItem);
 // };
 // Storage.prototype.removeItem = function(item){
-// 	this.items = item;
+// 	this.items.splice(1, 1);
 // };
 // // Пиши код выше этой строки
 // const storage = new Storage(['Нанитоиды', 'Пролонгер', 'Антигравитатор']);
@@ -235,3 +236,195 @@
 // console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
 // storage.removeItem('Пролонгер');
 // console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
+
+
+////// 7 task 5 module ///////////////
+
+// function StringBuilder(baseValue) {
+// 	this.value = baseValue; 
+// };
+// StringBuilder.prototype.getValue = function () {
+// 	return this.value;
+// };
+// StringBuilder.prototype.padEnd = function (str) {
+// 	this.value = this.value + `${str}`;
+// };
+// StringBuilder.prototype.padStart = function (str) {
+// 	this.value = `${str}` + this.value;
+// };
+// StringBuilder.prototype.padBoth = function (str) {
+// 	this.value = `${str}` + this.value + `${str}`;
+// };
+
+// // Пиши код выше этой строки
+// const builder = new StringBuilder('.');
+// console.log(builder.getValue()); // '.'
+// builder.padStart('^');
+// console.log(builder.getValue()); // '^.'
+// builder.padEnd('^');
+// console.log(builder.getValue()); // '^.^'
+// builder.padBoth('=');
+// console.log(builder.getValue()); // '=^.^='
+
+//////////// 12 task 5 module ///////////////
+
+// class Storage {
+//     #items;
+//     constructor(items) {
+//         this.#items = items;
+//     }
+//     getItems() {
+//         return this.#items;
+//     }
+//     addItem(newItem) {
+//         this.#items.push(newItem);
+//     }
+//     removeItem(item) {
+//         const itemIndex = this.#items.indexOf(item);
+//         this.#items.splice(itemIndex, 1);
+//     }
+// }
+//     // Пиши код выше этой строки
+//   const storage = new Storage(["Нанитоиды", "Пролонгер", "Антигравитатор"]);
+//   console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+//   storage.addItem("Дроид");
+//   console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+//   storage.removeItem("Пролонгер");
+//   console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
+  
+
+/////////////// 13 task 5 module //////////////
+
+// class StringBuilder {
+//     #value;
+//     constructor(baseValue) {
+//         this.#value = baseValue;
+//     }
+//     getValue() {
+//         return this.#value;
+//     }
+//     padEnd(str) {
+//         this.#value += str;
+//     }
+//     padStart(str) {
+//         this.#value = str + this.#value;
+//     }
+//     padBoth(str) {
+//         this.padStart(str);
+//         this.padEnd(str);
+//     }
+// }
+//     // Пиши код выше этой строки
+//   const builder = new StringBuilder('.');
+//   console.log(builder.getValue()); // '.'
+//   builder.padStart('^');
+//   console.log(builder.getValue()); // '^.'
+//   builder.padEnd('^');
+//   console.log(builder.getValue()); // '^.^'
+//   builder.padBoth('=');
+//   console.log(builder.getValue()); // '=^.^='
+
+
+///////////////// 14 task 5 module /////////////
+
+// class Car {
+//     #model;
+//     #price;
+//     #brand;
+  //     constructor({ brand, model, price }) {
+//       this.#brand = brand;
+//       this.#model = model;
+//       this.#price = price;
+//     }
+  //     get brand() {
+//       return this.#brand;
+//     }
+  //     set brand(newBrand) {
+//       this.#brand = newBrand;
+//     }
+  //     get model() {
+//       return this.#model;
+//     }
+  //     set model(newModel) {
+//       this.#model = newModel;
+//     }
+  //     get price() {
+//       return this.#price;
+//     }
+  //     set price(newPrice) {
+//       this.#price = newPrice;
+//     }
+//   }
+  
+
+///////// 15 task 5 module /////////////
+
+// class Car {
+//     // Пиши код ниже этой строки
+//     static MAX_PRICE = 50000;
+//     #price;
+//     constructor({ price }) {
+//       this.#price = price;
+//     }
+//     get price() {
+//       return this.#price;
+//     }
+//     set price(newPrice) {
+//         if(Car.MAX_PRICE <= newPrice) {
+//             return;
+//         }
+//         this.#price = newPrice;
+//       }
+//     // Пиши код выше этой строки
+//   }
+//   const audi = new Car({price: 35000});
+//   console.log(audi.price); // 35000
+  
+//   audi.price = 49000;
+//   console.log(audi.price); // 49000
+  
+//   audi.price = 51000;
+//   console.log(audi.price); // 49000
+
+//////////////////// 16 task 5 module //////////
+
+// class Car {
+//     static #MAX_PRICE = 50000;
+//     // Пиши код ниже этой строки
+//     static checkPrice(price) {
+//         if(price > Car.#MAX_PRICE) {
+//           return 'Внимание! Цена превышает допустимую.';
+//       }
+//           return 'Всё хорошо, цена в порядке.';
+//     }
+//     // Пиши код выше этой строки
+//     constructor({ price }) {
+//       this.price = price;
+//     }
+//   }
+//   const audi = new Car({ price: 36000 });
+//   const bmw = new Car({ price: 64000 });
+//   console.log(Car.checkPrice(audi.price)); // Всё хорошо, цена в порядке.
+//   console.log(Car.checkPrice(bmw.price)); // Внимание! Цена превышает допустимую.
+
+/////////////17 task 5 module ///////////
+
+// class User {
+//     email;
+//       constructor(email) {
+//       this.email = email;
+//     }
+//       get email() {
+//       return this.email;
+//     }
+//       set email(newEmail) {
+//       this.email = newEmail;
+//     }
+//   }
+//   // Пиши код ниже этой строки
+//   class Admin extends User {
+//       static AccessLevel = {
+//           BASIC: "basic",
+//           SUPERUSER: "superuser",
+//       };
+//   }
